017ba2e0d45af69a6747588616835d75481ccca4
Roshan Praveen Shetty <47758717+RoshanPShetty@users.noreply.github.com>
2021-03-30 02:01:42 -0400

    Created and edited 4 js files and 1 scss file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 
 function App() {
+  const [animeList, SetAnimeList] = useState([]);
+  const [topAnime, SetTopAnime] = useState([]);
+  const [search, SetSearch] = useState("");
+
+  const GetTopAnime = async () => {
+    // Retrieve 50 anime from the 1st page of the API
+    const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`).then(res => res.json());
+
+    // Slices the array to give the first 5 anime
+    SetTopAnime(temp.top.slice(0, 13));
+  }
+
+  const HandleSearch = e => {
+    e.preventDefault();
+
+    FetchAnime(search);
+  }
+
+  const FetchAnime = async (query) => {
+    const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc`).then(res => res.json());
+
+    SetAnimeList(temp.results);
+  }
+
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          SetSearch={SetSearch}
+          animeList={animeList}
+
+        />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// Not a required header
+import React from 'react'
+
+function Header() {
+    return (
+        <header><h1>Anime<strong>Recommender</strong></h1></header>
+    )
+
+}
+
+export default Header
